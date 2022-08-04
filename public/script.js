2bccf0cde4067ed9c9561e9ff19ed22fa4e15c7c
EC2 Default User <ec2-user@ip-172-31-37-241.ap-northeast-1.compute.internal>
2022-08-04 07:11:34 +0000

    [Feat] remove redundant msg

--- public/script.js
+++ public/script.js
@@ -4,9 +4,9 @@ let socket = new WebSocket("wss://domingoos.store");
 document.getElementById("submit-btn").addEventListener('click', ()=>{
     const outgoingMessage = document.getElementById("input-text").value;
     socket.send(outgoingMessage);
-    let messageElem = document.createElement('div');
-    messageElem.textContent = outgoingMessage;
-    document.getElementById('messages').prepend(messageElem);
+    //let messageElem = document.createElement('div');
+    //messageElem.textContent = outgoingMessage;
+    //document.getElementById('messages').prepend(messageElem);
 })
   
   // message received - show the message in div#messages
